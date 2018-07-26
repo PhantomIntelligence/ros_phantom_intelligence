dcb3749190d2b76b240ea7ddb768e81a6e47ca5d
JBHarvey <jean.benoit.harvey@outlook.com>
2018-07-26 18:16:14 -0400

    Create base for ROS tutorial and catkin package

---
+++ tutorial/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jb/Dropbox/work/phantom-intelligence/ros_phantom_intelligence/tutorial/catkin_ws/src
+# Build directory: /home/jb/Dropbox/work/phantom-intelligence/ros_phantom_intelligence/tutorial/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ tutorial/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/jb/Dropbox/work/phantom-intelligence/ros_phantom_intelligence/tutorial/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/melodic".split(';') if "/opt/ros/melodic" != "" else []
