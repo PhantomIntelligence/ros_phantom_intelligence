cbaa7a39d82a529f71c8137ae6b21e79fa06093c
JBHarvey <jean.benoit.harvey@outlook.com>
2018-07-30 14:09:44 -0400

    Add gitignore

--- tutorial/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/jb/Dropbox/work/phantom-intelligence/ros_phantom_intelligence/tutorial/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/jb/Dropbox/work/phantom-intelligence/ros_phantom_intelligence/tutorial/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/jb/Dropbox/work/phantom-intelligence/ros_phantom_intelligence/tutorial/catkin_ws/devel;/opt/ros/melodic" != "" else []
