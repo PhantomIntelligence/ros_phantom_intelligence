428912f75b08aa953011053a535f4f6a9a6f7b6c
JBHarvey <jean.benoit.harvey@outlook.com>
2018-08-01 19:49:46 -0400

    Base of connection between repos

---
+++ cmake_module/SpiritSensorGateway.cmake
@@ -0,0 +1,34 @@
+
+macro(fetchSpiritSensorGateway _download_module_path _download_root)
+    message(STATUS "Building SpiritSensorGateway")
+    message(STATUS "Gateway download directory: ${_download_root}")
+
+    set(SPIRIT_SENSOR_GATEWAY_DOWNLOAD_ROOT ${_download_root})
+    configure_file(
+            ${_download_module_path}/SpiritSensorGateway_download.cmake.in
+            ${_download_root}/CMakeLists.txt
+            @ONLY
+    )
+    unset(SPIRIT_SENSOR_GATEWAY_DOWNLOAD_ROOT)
+
+    execute_process(
+            COMMAND
+            "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+            WORKING_DIRECTORY ${_download_root}
+    )
+    execute_process(
+            COMMAND
+            "${CMAKE_COMMAND}" --build .
+            WORKING_DIRECTORY ${_download_root}
+    )
+
+    # Adds the targets: SpiritSensorGateway
+    add_subdirectory(
+            ${_download_root}/SpiritSensorGateway-src
+            ${_download_root}/SpiritSensorGateway-build
+    )
+    find_package(SpiritSensorGateway REQUIRED)
+
+    message(STATUS "Done with SpiritSensorGateway")
+    message(STATUS)
+endmacro()
---
+++ cmake_module/setupCMakeMacros.cmake
@@ -0,0 +1,10 @@
+
+macro(setup_cmake_macros _source_dir _binary_dir)
+
+    set(CMAKE_MACRO_SOURCE_DIR ${_source_dir})
+    set(CMAKE_MACRO_BINARY_DIR ${_binary_dir})
+
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/cmake_module/")
+
+    include(cmake_module/SpiritSensorGateway.cmake)
+endmacro()
